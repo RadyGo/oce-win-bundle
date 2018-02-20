997176ee1cb2aa988214c0bbf34956925fc24f26
Thomas Paviot <tpaviot@gmail.com>
2018-02-20 07:15:19 +0100

    Updated depends.cmake to occt720

--- Depends.cmake
+++ Depends.cmake
@@ -1,17 +1,16 @@
 message("Setting oce-win-bundle depends...")
 
 # Add dependencies between OCE and oce-win-bundle
-if(OCE_WITH_FREEIMAGE)
+if(USE_FREEIMAGE)
     add_dependencies( TKService FreeImage FreeImagePlus )
     if(NOT OCE_BUILD_SHARED_LIB)
 		   TARGET_COMPILE_DEFINITIONS(TKService PRIVATE "-DFREEIMAGE_LIB")
 	  endif()
 endif()
 
-if (OCE_VISUALISATION)
-    add_dependencies( TKOpenGl freetype )
-    if (OCE_WITH_GL2PS)
-          add_dependencies( TKOpenGl gl2ps )
-    endif()
+#if (OCE_VISUALISATION)
+add_dependencies( TKOpenGl freetype )
+if (USE_GL2PS)
+      add_dependencies( TKOpenGl gl2ps )
 endif()
-
+#endif()
